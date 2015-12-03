e6b06c8dc3183640d27d9c521afb39d0e7e4a30b
claudia aguirre <claudialexaa@gmail.com>
2015-12-02 22:28:17 -0500

    New Website
    Virtual tour, mobile fixed except home on android

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$("#demosMenu").change(function(){
+	  window.location.href = $(this).find("option:selected").attr("id") + '.html';
+	});
+});
+
+/* Applying the "open" class when you click the Menu button, which triggers the mobile menu to open/close; Toggle Menu/Close menu text. */
+$(document).ready(function() {
+  $('.mobile-button a').click(function(e) {
+	$(this).parent().parent().toggleClass('open');
+  $(this).html($(this).html() == 'Close Menu' ? 'Menu' : 'Close Menu');
+    e.preventDefault();
+  });
+});
